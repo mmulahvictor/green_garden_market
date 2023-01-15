8629c2d854c2d6d13759eaeaf0c537ea766ea57f
mmulahvictor <mmulahvictor@gmail.com>
2023-01-15 19:45:04 +0300

    products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,45 @@
-class ProductsController < ApplicationController
+class Api::V1::ProductsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_product, only: [:show, :update, :destroy]
+
+  def index
+    @products = Product.all
+    render json: @products
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      render json: @product, status: :created
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: @product
+  end
+
+  def update
+    if @product.update(product_params)
+      render json: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :image, :farmer_id, :category_id)
+  end
 end
